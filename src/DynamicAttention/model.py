13b8c37eb769736d128be9b6b43500a12ef9596f
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-05-15 09:48:37 -0400

    Moved attention into it's own class.

--- src/DynamicAttention/model.py
+++ src/DynamicAttention/model.py
@@ -4,6 +4,46 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
 
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super().__init__()
+        self.method = method
+        self.hidden_size = hidden_size
+
+        if self.method == 'general':
+            self.attn = nn.Linear(self.hidden_size, hidden_size)
+
+        elif self.method == 'concat':
+            self.attn = nn.Linear(self.hidden_size * 2, hidden_size)
+            self.other = nn.Parameter(torch.FloatTensor(1, hidden_size))
+
+    def forward(self, hidden, objs, device):
+        num_objs = objs.shape[1]
+        attn_energies = torch.zeros(num_objs).to(device)
+        # calculate energies for each encoder output
+        for i in range(num_objs):
+            attn_energies[i] = self.score(hidden, objs[:, i])
+
+        # normalize energies to weights in range 0 to 1
+        return F.softmax(attn_energies, dim=0).unsqueeze(0)
+
+    def score(self, hidden, obj):
+        if self.method == 'dot':
+            energy = hidden.dot(obj)
+            return energy
+        
+        elif self.method == 'general':
+            energy = self.attn(obj)
+            energy = energy.squeeze()
+            hidden = hidden.squeeze()
+            energy = hidden.dot(energy)
+            return energy
+
+        elif self.method == 'concat':
+            energy = self.attn(torch.cat((hidden, encoder_output), 1))
+            energy = self.other.dot(energy)
+            return energy
+
 class DynamicAttention(nn.Module):
     """Single stream model.
     Args:
@@ -69,7 +109,7 @@ class DynamicAttention(nn.Module):
         # define layers
         self.embedding = nn.Linear(num_fts, hidden_size)
         # attention layer
-        self.attn = nn.Linear(hidden_size, hidden_size)
+        self.attn = Attn('general', hidden_size)
         self.attn_combine = nn.Linear(hidden_size*2, hidden_size)
         # add lstm layer
         self.lstm = nn.LSTM(hidden_size, hidden_size, rnn_layers)
@@ -79,59 +119,37 @@ class DynamicAttention(nn.Module):
     def forward(self, inp_frame, inp_objs, state, device):
         # break state into hidden state and cell state
         h, c = state
-#        print('inp_frame:', inp_frame.shape)
-#        print('inp_objs:', inp_objs.shape)
-#        print('h:', h.shape)
-#        print('c:', c.shape)
         # pass through CNN + embedding
         emb_frame = self.embedding(self.cnn.forward(inp_frame))
-#        print('emb_frame:', emb_frame.shape)
         window_size = inp_objs.shape[1]
         inp_objs = inp_objs.view(-1, 3, 224, 224)
         emb_objs = self.embedding(self.cnn.forward(inp_objs))
         emb_objs = emb_objs.view(self.batch_size, window_size, -1)
-#        print('emb_objs:', emb_objs.shape)
 
         # for attention for each object
-        num_objs = emb_objs.shape[1]
-        attn_energies = torch.zeros(num_objs).to(device)
-        # calculate energies for each object
-        for i in range(num_objs):
-            attn_energies[i] = self.score(h[-1], emb_objs[:, i])
-
-#        print('attn_energies:', attn_energies.shape)
-        # normalize energies
-        attn_weights = F.softmax(attn_energies, dim=0).unsqueeze(0)
-#        print('attn_weights:', attn_weights.shape)
+        attn_weights = self.attn(h[-1], emb_objs, device)
 
         # context
         context = torch.mm(attn_weights, emb_objs[0])
-#        print('context:', context.shape)
 
         # combine
         output = torch.cat((emb_frame, context), 1)
-#        print('output:', output.shape)
         output = self.attn_combine(output)
-#        print('output:', output.shape)
 
         # pass through RNN
         output = output.unsqueeze(0)
         output, state = self.lstm(output, state)
-#        print('output:', output.shape)
-#        print('h:', state[0].shape)
-#        print('c:', state[1].shape)
 
         # pass through fc layer
         output = self.fc(output).squeeze(0)
-#        print('output:', output.shape)
         return output, state
 
 
     def init_hidden(self, device):
         h_0 = torch.zeros(self.rnn_layers, self.batch_size, self.hidden_size)
         c_0 = torch.zeros(self.rnn_layers, self.batch_size, self.hidden_size)
-        h_0 = h_0.to(device)
-        c_0 = c_0.to(device)
+        h_0.to(device)
+        c_0.to(device)
         return (h_0, c_0)
 
     def score(self, hidden, obj):
@@ -144,31 +162,33 @@ class DynamicAttention(nn.Module):
 
 def main():
     """Test Function."""
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = 'AlexNet'
     batch_size = 1
     hidden_size = 512
     rnn_layers = 1
     net = DynamicAttention(model, batch_size, hidden_size, rnn_layers, 
             pretrained=False, finetuned=False)
+    net.to(device)
     print(net)
 
     window_size = 10
     max_objects = 20
     X_frames = torch.randn(window_size, batch_size, 3, 224, 224)
     X_objs = torch.randn(window_size, batch_size, max_objects, 3, 224, 224)
+    X_frames.to(device)
+    X_objs.to(device)
     print('X_frames:', X_frames.shape)
     print('X_objs:', X_objs.shape)
 
     # initialize hidden
-    state = net.init_hidden()
+    state = net.init_hidden(device)
     # for each time step
     for i in range(window_size):
         frame = X_frames[i]
         objs = X_objs[i]
-        output, state = net.forward(frame, objs, state)
+        output, state = net.forward(frame, objs, state, device)
         print('output:', output.shape)
-
-
 
 if __name__ == '__main__':
     main()
