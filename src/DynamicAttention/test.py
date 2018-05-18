b72fcc4a156f6587a13bf359fa58121fc2331e50
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-05-18 12:49:49 -0400

    Use display flag.

--- src/DynamicAttention/test.py
+++ src/DynamicAttention/test.py
@@ -8,18 +8,18 @@ from model import DynamicAttention
 def main():
     """MAIN FUNCTION."""
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    data_path = 'data/labels_done.txt'
+    data_path = 'data/labels.txt'
     batch_size = 1
     num_workers = 0
     sequence_len = 100
     ws = 10  # window_size
 
     dataloader, dataset_size = get_loader(data_path, sequence_len, batch_size,
-            num_workers)
+            num_workers, display=True)
     print('Dataset size:', dataset_size)
 
     it = iter(dataloader)
-    for _ in range(10):
+    for _ in range(1):
         batch = next(it)
 
     X_frames = batch['X_frames'].transpose(0, 1).to(device)
@@ -28,7 +28,7 @@ def main():
     print('X_objs:', X_objs.shape)
 
     # create model
-    model = 'VGGNet11'
+    model = 'AlexNet'
     batch_size = 1
     hidden_size = 512
     rnn_layers = 2
@@ -36,7 +36,7 @@ def main():
             pretrained=True, finetuned=False).to(device)
     net.load_state_dict(torch.load('data/model_params.pkl'))
 
-    with open('outputs_positive_000512.txt', 'w') as f:
+    with open('outputs.txt', 'w') as f:
         for i in range(1, sequence_len+1):
             inp_frames = X_frames[i-ws if i-ws > 0 else 0: i]
             inp_objs = X_objs[i-ws if i-ws > 0 else 0: i]
