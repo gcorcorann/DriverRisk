da5d80bb43b8c7cf7ee1f96d05353f29b7ae80f5
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-05-21 18:46:20 -0400

    Added validation.

--- src/DynamicAttention/train2.py
+++ src/DynamicAttention/train2.py
@@ -2,22 +2,22 @@ import time
 import torch
 import copy
 
-def train_network(net, dataloader, dataset_size, criterion, optimizer, 
+def train_network(net, dataloaders, dataset_sizes, criterion, optimizer, 
         max_epochs):
     """Train network.
 
     Args:
         net (torchvision.models):                   network to train
-        dataloader (torch.utils.data.DataLoader):   dataloader
-        dataset_size (int):                         size of dataset
+        dataloaders (torch.utils.data.DataLoader):  dataloaders
+        dataset_sizes (int):                        size of datasets
         criterion (torch.nn.modules.loss):          loss function
         optimizer (torch.optim):    optimization algorithm
         max_epochs (int):           maximum number of epochs used for training
 
     Returns:
-        best_acc (int):     best training accuracy
-        losses (list):      training losses
-        accuracies (list):  training accuracies
+        best_acc (int):     best validation accuracy
+        losses (list):      losses
+        accuracies (list):  accuracies
     """
     # start timer
     start = time.time()
@@ -26,75 +26,82 @@ def train_network(net, dataloader, dataset_size, criterion, optimizer,
     # store network to cpu/gpu
     net = net.to(device)
     # training losses + accuracies
-    losses, accuracies = [], []
+    losses = {'Train': [], 'Valid': []}
+    accuracies = {'Train': [], 'Valid': []}
     best_acc = 0 
     best_net_wts = copy.deepcopy(net.state_dict())
     # when to stop training
     patience = 0
-    # for each training epoch
+    # for each epoch
     for epoch in range(max_epochs):
         print()
         print('Epoch', epoch)
         print('-' * 8)
-        # set network to training mode
-        net.train()
-        # used for losses + accuracies
-        running_loss = 0
-        running_correct = 0
-        # iterate over data
-        for i, data in enumerate(dataloader):
-            # get the inputs and labels, reshape [seq_len, batch_size, *]
-            X_frames, X_objs, y = data
-            X_frames = X_frames.transpose(0, 1).to(device)
-            X_objs = X_objs.transpose(0, 1).to(device)
-            y = y.transpose(0, 1).to(device)
-            # zero the parameter gradients
-            optimizer.zero_grad()
-            # input size
-            sequence_len, batch_size = X_frames.shape[:2]
-            # initialize hidden states
-            states = net.init_states(batch_size, device)
-            # for each timestep
-            loss = 0
-            correct = 0
-            for t in range(sequence_len):
-                frame = X_frames[t]
-                objs = X_objs[t]
-                # forward pass
-                output, states, attn = net.forward(frame, objs, states)
-                if i == 0 and t == 10:
-                    print('objs:', objs[:, :, 0, 100, 100])
-                    print('attn:', attn)
-                    print()
-                # loss + prediction
-                loss += criterion(output, y[t])
-                _, y_pred = torch.max(output, 1)
-                correct += (y_pred == y[t]).sum().item()
+        # each epoch has a training and validation phase
+        for phase in ['Train', 'Valid']:
+            if phase == 'Train':
+                net.train() # set network to training mode
+            else:
+                net.eval()  # set network to evaluation mode
 
-            # backwards + optimize
-            loss.backward()
-            optimizer.step()
-            # statistics
-            running_loss += loss.item() * batch_size / sequence_len
-            running_correct += correct / sequence_len
-                    
-        epoch_loss = running_loss / dataset_size
-        epoch_acc = running_correct / dataset_size
-        print('Training Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, 
-            epoch_acc))
-        # store stats
-        losses.append(epoch_loss)
-        accuracies.append(epoch_acc)
-        # increase patience
-        patience += 1
-        # save best accuracy
-        if epoch_acc > best_acc:
-            best_acc = epoch_acc
-            best_net_wts = copy.deepcopy(net.state_dict())
-            patience = 0
+            # used for losses + accuracies
+            running_loss = 0
+            running_correct = 0
+            # iterate over data
+            for i, data in enumerate(dataloaders[phase]):
+                # get the inputs and labels, reshape [seq_len, batch_size, *]
+                X_frames, X_objs, y = data
+                X_frames = X_frames.transpose(0, 1).to(device)
+                X_objs = X_objs.transpose(0, 1).to(device)
+                y = y.transpose(0, 1).to(device)
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                # input size
+                sequence_len, batch_size = X_frames.shape[:2]
+                # initialize hidden states
+                states = net.init_states(batch_size, device)
+                # for each timestep
+                loss = 0
+                correct = 0
+                # track history if in training phase
+                with torch.set_grad_enabled(phase == 'Train'):
+                    for t in range(sequence_len):
+                        frame = X_frames[t]
+                        objs = X_objs[t]
+                        # forward pass
+                        output, states, _ = net.forward(frame, objs, states)
+                        # loss + prediction
+                        loss += criterion(output, y[t])
+                        _, y_pred = torch.max(output, 1)
+                        correct += (y_pred == y[t]).sum().item()
 
-        if patience == 10:
-            break
+                    # backwards + optimize only if in training phase
+                    if phase == 'Train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * batch_size / sequence_len
+                running_correct += correct / sequence_len
+                        
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_correct / dataset_sizes[phase]
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, 
+                epoch_acc))
+            # store stats
+            losses[phase].append(epoch_loss)
+            accuracies[phase].append(epoch_acc)
+            # if in validation phase
+            if phase == 'Valid':
+                patience += 1  # increase patience
+                # save best accuracy
+                if epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_net_wts = copy.deepcopy(net.state_dict())
+                    patience = 0
+    
+                if patience == 10:
+                    break
 
     # print elapsed time
     time_elapsed = time.time() - start
