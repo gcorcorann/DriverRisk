623133b34e9778aaa7466b2bdd930970d678685c
Gary Corcoran <gary.corcoran@mail.mcgill.ca>
2018-05-24 14:40:41 -0400

    Save best model.

--- src/DynamicAttention/main2.py
+++ src/DynamicAttention/main2.py
@@ -36,39 +36,41 @@ def main():
     # dataloader parameters
     train_path = 'data/train_data.txt'
     valid_path = 'data/valid_data.txt'
-    batch_sizes = [2, 5]
-    num_workers = [2, 1]
+    batch_size = 3
+    num_workers = 2
     # network parameters
     hidden_size = 512
     rnn_layers = 2
     pretrained = True
     # training parameters
     learning_rate = 1e-4
-    max_epochs = 10
+    max_epochs = 100
     criterion = nn.CrossEntropyLoss()
 
     # for each hyper-parameter
-    for i in range(2):
-        bs, nw = batch_sizes[i], num_workers[i]
-        # get dataloaders
-        dataloaders, dataset_sizes = get_loaders(train_path, valid_path, 
-                bs, nw, shuffle=True)
-        print('Dataset Sizes:', dataset_sizes)
-    
-        # create network object
-        net = DynamicAttention(hidden_size, rnn_layers, pretrained)
-        # create optimizer
-        p = list(net.embedding.parameters()) + list(net.attn.parameters()) \
-                + list(net.attn_combine.parameters()) \
-                + list(net.lstm.parameters()) + list(net.fc.parameters())
-        optimizer = torch.optim.Adam(p, learning_rate)
-    
-        # train the network
-        best_acc, losses, accuracies = train_network(net, dataloaders,
-                dataset_sizes, criterion, optimizer, max_epochs)
-        # plot statistics
-        print('Best Validation Accuracy:', round(best_acc*100,2))
-        plot_data(losses, accuracies, 'outputs/{}.png'.format(i))
+    best_acc = 0
+    # get dataloaders
+    dataloaders, dataset_sizes = get_loaders(train_path, valid_path, 
+            batch_size, num_workers, shuffle=True)
+    print('Dataset Sizes:', dataset_sizes)
+
+    # create network object
+    net = DynamicAttention(hidden_size, rnn_layers, pretrained)
+    # create optimizer
+    p = list(net.embedding.parameters()) + list(net.attn.parameters()) \
+            + list(net.attn_combine.parameters()) \
+            + list(net.lstm.parameters()) + list(net.fc.parameters())
+    optimizer = torch.optim.Adam(p, learning_rate)
+
+    # train the network
+    net, valid_acc, losses, accuracies = train_network(net, dataloaders,
+            dataset_sizes, criterion, optimizer, max_epochs)
+    # plot statistics
+    print('Best Validation Accuracy:', round(valid_acc*100,2))
+    plot_data(losses, accuracies, 'outputs/{}.png'.format(i))
+    # save best network to disk
+    if valid_acc > best_acc:
+        torch.save(net.state_dict(), 'outputs/net_params.pkl')
 
 if __name__ == '__main__':
     main()
